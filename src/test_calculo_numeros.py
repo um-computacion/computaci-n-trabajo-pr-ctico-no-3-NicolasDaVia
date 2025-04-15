f1937f742d28e48067f28fb97e0fb68f06abad29
NicolasDaVia <n.davia@alumno.um.edu.ar>
2025-04-15 14:44:32 +0000

    Agregar tests para ingreso de números válidos

---
+++ src/test_calculo_numeros.py
@@ -0,0 +1,11 @@
+import unittest
+from unittest.mock import patch
+from calculo_numeros import ingrese_numero
+from excepcion import NegativeNumberError
+
+class TestIngresoNumero(unittest.TestCase):
+
+    @patch('builtins.input', return_value='100')
+    def test_numero_valido(self, mocked_input):
+        resultado = ingrese_numero()
+        self.assertEqual(resultado, 100.0)
